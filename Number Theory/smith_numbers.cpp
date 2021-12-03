abdb43da1d6f320fb784fddae8650d293cf5433c
Atharva Pingale <76089814+atharva0300@users.noreply.github.com>
2021-12-03 17:11:45 +0530

    smith numbers added

---
+++ Number Theory/smith_numbers.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/* 
+    Given a number n, the task is to find out whether this number 
+    is smith or not. A Smith Number is a composite number whose sum 
+    of digits is equal to the sum of digits in its prime factorization.
+*/
+
+/*
+    Examples:
+
+Input  : n = 4
+Output : Yes
+Prime factorization = 2, 2  and 2 + 2 = 4
+Therefore, 4 is a smith number
+
+Input  : n = 6
+Output : No
+Prime factorization = 2, 3  and 2 + 3 is
+not 6. Therefore, 6 is not a smith number
+
+Input   : n = 666
+Output  : Yes
+Prime factorization = 2, 3, 3, 37 and
+2 + 3 + 3 + (3 + 7) = 6 + 6 + 6 = 18
+Therefore, 666 is a smith number
+
+Input   : n = 13
+Output  : No
+Prime factorization = 13 and 13 = 13,
+But 13 is not a smith number as it is not
+a composite number
+*/
+int main()
+{
+
+
+
+    return 0;
+}
